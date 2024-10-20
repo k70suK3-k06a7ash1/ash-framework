b4636f1f8983a148cc31f8c6cddf3e1a2140d23f
2814109 <2814109k@gmail.com>
2024-10-20 15:32:14 +0900

    chore

---
+++ config/config.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :helpdesk, :ash_domains, [Helpdesk.Support]
+
+config :ash,
+  include_embedded_source_by_default?: false,
+  default_page_type: :keyset
+
+config :ash, :policies,
+  no_filter_static_forbidden_reads?: false
