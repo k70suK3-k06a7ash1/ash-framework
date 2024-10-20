839ad60b1a83410a5116ab3dcef4d43cbdc1e66d
2814109 <2814109k@gmail.com>
2024-10-20 15:54:35 +0900

    format

--- config/config.exs
+++ config/config.exs
@@ -6,5 +6,4 @@ config :ash,
   include_embedded_source_by_default?: false,
   default_page_type: :keyset
 
-config :ash, :policies,
-  no_filter_static_forbidden_reads?: false
+config :ash, :policies, no_filter_static_forbidden_reads?: false
--- lib/helpdesk/support.ex
+++ lib/helpdesk/support.ex
@@ -2,6 +2,6 @@ defmodule Helpdesk.Support do
   use Ash.Domain
 
   resources do
-    resource Helpdesk.Support.Ticket
+    resource(Helpdesk.Support.Ticket)
   end
 end
