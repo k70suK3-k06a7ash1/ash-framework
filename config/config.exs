4fa58c39aa58e04dda93ae4aa32c57d338127d85
2814109 <2814109k@gmail.com>
2024-10-21 03:56:18 +0900

    chore

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,6 @@ config :helpdesk, :ash_domains, [MyApp.Accounts, Helpdesk.Support]
 config :ash,
   include_embedded_source_by_default?: false,
   default_page_type: :keyset,
-  custom_types: [ticket_status: MyApp.Support.Ticket.Types.Status]
+  custom_types: [ticket_status: Helpdesk.Support.Ticket.Types.Status]
 
 config :ash, :policies, no_filter_static_forbidden_reads?: false
